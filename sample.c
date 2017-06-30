98c9370d2ce93e9b8461b6368257dd0ea7bbe6be
sudheer5c4 <sudheeratonline@gmail.com>
2017-06-30 10:37:50 +0000

    commiting to test stash

--- sample.c
+++ sample.c
@@ -1,2 +1,10 @@
 hi this is a simple c prg and 
 	i am going to delete this branch with out merging.
+
+
+
+
+
+
+
+-----------------------------------------------------------------------------------
